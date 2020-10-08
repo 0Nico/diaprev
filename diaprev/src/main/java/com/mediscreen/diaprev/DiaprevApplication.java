ca4ded68afb3a92ab7caecbd5dc397229b131601
Nicolas Dupuy <nicolas.dupuy@mimbus.com>
2020-10-09 01:45:57 +0200

    Add maven-base project

---
+++ diaprev/src/main/java/com/mediscreen/diaprev/DiaprevApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diaprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaprevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaprevApplication.class, args);
+	}
+
+}
---
+++ diaprev/src/test/java/com/mediscreen/diaprev/DiaprevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diaprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaprevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
